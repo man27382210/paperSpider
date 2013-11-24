efba38da70accfd9ab90583c7f16b3a652b9335e
man27382210 <man27382210@gmail.com>
2013-11-25 01:12:59 +0800

    add new paper spider

--- citeSeer/spiders/paperInfoParse.py
+++ citeSeer/spiders/paperInfoParse.py
@@ -5,7 +5,7 @@ from citeSeer.items import CiteseerItem
 import urlparse
 
 class citeTestSpider(BaseSpider):
-    name = "citeSearch"
+    name = "citePaperParse"
     allowed_domains = ["citeseerx.ist.psu.edu"]
     def __init__(self, *args, **kwargs):
         super(citeTestSpider, self).__init__(*args, **kwargs)
@@ -16,7 +16,7 @@ class citeTestSpider(BaseSpider):
         # self.start_urls = ["http://citeseerx.ist.psu.edu/search?q=Extracting+Noun+Phrases+in+Subject+and+Object+Roles+for+Exploring+Text+Semantics&submit=Search&sort=rlv&t=doc"]
     # start_urls = ['http://citeseerx.ist.psu.edu/search?q=Extracting+Noun+Phrases+in+Subject+and+Object+Roles+for+Exploring+Text+Semantics&submit=Search&sort=rlv&t=doc']
 
-    def start_requests(self):  
+    def start_requests(self):               
         checkRequest = Request( 
             url      = self.start_urls, 
             meta     = {"test":"first"},
@@ -39,6 +39,7 @@ class citeTestSpider(BaseSpider):
             ).replace(meta = {"test":"not first"})
             return [checkRequest]
         else:
+            print "END LOOPING"
             return self.items
 
     def parse_searchTitle(self, sites):
