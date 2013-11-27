153b1b62b8b15526c5da3ca7c4258cb7aab93ff7
man27382210 <man27382210@gmail.com>
2013-11-28 02:44:32 +0800

    add cite id

--- citeSeer/spiders/paperInfoParseTwo.py
+++ citeSeer/spiders/paperInfoParseTwo.py
@@ -10,7 +10,7 @@ class citePaperSpider(BaseSpider):
     pipelines = ['MongoDBPipeline']
     def __init__(self, *args, **kwargs):
         super(citePaperSpider, self).__init__(*args, **kwargs)
-        self.start_urls = "http://citeseerx.ist.psu.edu"+kwargs.get('start_url')
+        self.start_urls = "http://citeseerx.ist.psu.edu"+kwargs.get('start_url') 
         # self.url = kwargs.get('start_url')
         # self._id = kwargs.get('start_url').split('=')[1]
         # self.title = ""
@@ -29,24 +29,28 @@ class citePaperSpider(BaseSpider):
     def parserPaper(self, response):
         level = response.meta['level']
         print "level : %s " % level
-        if level < 6:
+        if level < 3:
             paper = CiteseerPaperItem()
             hxs = HtmlXPathSelector(response)
+            paper['url'] = response.url
+            paper['_id'] = paper['url'].split("=")[1]
+            paper['citebypaper'] = []
             paper['title'] = hxs.select('//div[@id="viewHeader"]/h2/text()').extract()[0]
-            refs = hxs.select('//div[@id="citations"]/table/tr')
-            paper['defpaper'] = self.parse_ref(refs)
-            # citeUrl = hxs.select('//div[@id="pager"]/a/@href').extract()[0]
-            # yield Request(url = "http://citeseerx.ist.psu.edu"+citeUrl, meta = {"paper":paper, "self":self}, callback = self.parser_cite)
-            yield paper
-            for ref in paper['defpaper']:
-                yield Request(url = "http://citeseerx.ist.psu.edu"+ref['url'], meta = {"level":level+1}, callback = self.parserPaper)
-            # yield self.extractData(paper)
-            # print "paper :%s " % paper
-            
-        else:
-            pass
-            # return self.extractData()
-            # return paper
+            try:
+                refs = hxs.select('//div[@id="citations"]/table/tr')
+                paper['defpaper'] = self.parse_ref(refs)
+                for ref in paper['defpaper']:
+                    yield Request(url = "http://citeseerx.ist.psu.edu"+ref['url'], meta = {"level":level+1}, callback = self.parserPaper)
+            except Exception, e:
+                print "No ref"
+                pass
+
+            try:
+                citeUrl = hxs.select('//div[@id="docOther"]/table/tr/td/a/@href').extract()[0]
+                yield Request(url = "http://citeseerx.ist.psu.edu"+citeUrl, meta = {"paper":paper}, callback = self.parser_cite)
+            except Exception, e:
+                print "No cite"
+                yield paper
 
     def parse_ref(self, refsInPaper):
         refsArray = []
@@ -60,35 +64,36 @@ class citePaperSpider(BaseSpider):
         
     def parser_cite(self, response):
         paper = response.meta['paper']
-        self = response.meta['self']
         hxs = HtmlXPathSelector(response)
         sites = hxs.select('//div[@class="result"]')
         paper['citebypaper'].extend(self.parse_citeTitle(sites))
-        urlNext = hxs.select('//div[@id="pager"]/a/@href').extract()[0]
-        if urlNext:
+        try:
+            urlNext = hxs.select('//div[@id="pager"]/a/@href').extract()[0]
             urlNext = "http://citeseerx.ist.psu.edu"+urlNext
-            checkRequest = Request(
-                url = urlNext,
-                paper = paper,
-                self = self,
-                callback = self.parser_cite
-            )
-            return [ checkRequest ]
-        else:
-            return self.extractData()
+            yield Request( url = urlNext, meta = {"paper":paper}, callback = self.parser_cite)
+        except Exception, e:
+            print "No next"
+            yield paper
+
+        # if urlNext:
+        #     urlNext = "http://citeseerx.ist.psu.edu"+urlNext
+        #     yield Request( url = urlNext, meta = {"paper":paper}, callback = self.parser_cite)
+        # else:
+        #     # yield self.extractData(paper)
+        #     yield paper
 
     def parse_citeTitle(self, cites):
         items = []
         for cite in cites:
             title = cite.select('h3/a[@class="remove doc_details"]/text() | h3/a[@class="remove doc_details"]/em/text()').extract()[0]
             url = cite.select('h3/a[@class="remove doc_details"]/@href').extract()[0]
-            item = {"title":title, "url":url}
+            citeID = url.split("=")[1]
+            item = {"title":title, "url":url, "_id":citeID}
             items.append(item)
         return items
 
 
     def extractData(self, paper):
-        print "paper : %s" % paper
         # paper = CiteseerPaperItem()
         # paper['_id'] = self._id
         # paper['title'] = self.title
